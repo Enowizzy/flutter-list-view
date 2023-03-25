ca03cb823a691bbc501da527ec2efc2f2c5655ab
Jeedie JideX <enockyona906@gmail.com>
2023-03-25 13:38:30 +0300

    implement onTap

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 class List extends StatelessWidget {
@@ -10,59 +12,74 @@ class List extends StatelessWidget {
 
   Widget getListView() {
     var listView = ListView(
-      children: const <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+      children:  <Widget>[
+         Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
           child: Card(
             child: ListTile(
-              leading: Icon(Icons.landscape),
-              title: Text('Landscape'),
-              subtitle: Text("Beautiful Landscape"),
-              trailing: Icon(Icons.wb_sunny),
+              onTap: (){
+                print('JEEDIEx');
+              },
+              leading: const Icon(Icons.landscape),
+              title: const Text('Landscape'),
+              subtitle: const Text("Beautiful Landscape"),
+              trailing: const Icon(Icons.wb_sunny),
+            ),
+          ),
+        ),
+         Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              onTap: (){
+                print('JEEDIEx');
+              },
+              leading: const Icon(Icons.laptop_chromebook),
+              title: const Text('Laptop'),
+              subtitle: const Text("Beautiful Laptop"),
+              trailing: const Icon(Icons.laptop),
+            ),
+          ),
+        ),
+         Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              onTap: (){
+                print('JEEDIEx');
+              },
+              leading: const Icon(Icons.phone_android),
+              title: const Text('Android Phone'),
+              subtitle: const Text("Amazing Phone"),
+              trailing: const Icon(Icons.phone_android),
+            ),
+          ),
+        ),
+         Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              onTap: (){
+                print('JEEDIEx');
+              },
+              leading: const Icon(Icons.tablet_android),
+              title: const Text('Tablet Phone'),
+              subtitle: const Text("Great Tablet"),
+              trailing: const Icon(Icons.phone_android),
             ),
           ),
         ),
         Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
           child: Card(
             child: ListTile(
-              leading: Icon(Icons.laptop_chromebook),
-              title: Text('Laptop'),
-              subtitle: Text("Beautiful Laptop"),
-              trailing: Icon(Icons.laptop),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-          child: Card(
-            child: ListTile(
-              leading: Icon(Icons.phone_android),
-              title: Text('Android Phone'),
-              subtitle: Text("Amazing Phone"),
-              trailing: Icon(Icons.phone_android),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-          child: Card(
-            child: ListTile(
-              leading: Icon(Icons.tablet_android),
-              title: Text('Tablet Phone'),
-              subtitle: Text("Great Tablet"),
-              trailing: Icon(Icons.phone_android),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-          child: Card(
-            child: ListTile(
-              leading: Icon(Icons.desktop_mac_outlined),
-              title: Text('Monitor'),
-              subtitle: Text("Beautiful Monitor"),
-              trailing: Icon(Icons.desktop_mac_outlined),
+              onTap: (){
+                print('JEEDIEx');
+              },
+              leading: const Icon(Icons.desktop_mac_outlined),
+              title: const Text('Monitor'),
+              subtitle: const Text("Beautiful Monitor"),
+              trailing: const Icon(Icons.desktop_mac_outlined),
             ),
           ),
         )
