08f8eb45b812862854e95ae9940d79942853c220
Jeedie JideX <enockyona906@gmail.com>
2023-03-25 13:08:32 +0300

    wrap with Card()

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -11,35 +11,45 @@ class List extends StatelessWidget {
   Widget getListView() {
     var listView = ListView(
       children: const <Widget>[
-        ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text('Landscape'),
-          subtitle: Text("Beautiful Landscape"),
-          trailing: Icon(Icons.wb_sunny),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.landscape),
+            title: Text('Landscape'),
+            subtitle: Text("Beautiful Landscape"),
+            trailing: Icon(Icons.wb_sunny),
+          ),
         ),
-        ListTile(
-          leading: Icon(Icons.laptop_chromebook),
-          title: Text('Laptop'),
-          subtitle: Text("Beautiful Laptop"),
-          trailing: Icon(Icons.laptop),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.laptop_chromebook),
+            title: Text('Laptop'),
+            subtitle: Text("Beautiful Laptop"),
+            trailing: Icon(Icons.laptop),
+          ),
         ),
-        ListTile(
-          leading: Icon(Icons.phone_android),
-          title: Text('Android Phone'),
-          subtitle: Text("Amazing Phone"),
-          trailing: Icon(Icons.phone_android),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.phone_android),
+            title: Text('Android Phone'),
+            subtitle: Text("Amazing Phone"),
+            trailing: Icon(Icons.phone_android),
+          ),
         ),
-        ListTile(
-          leading: Icon(Icons.tablet_android),
-          title: Text('Tablet Phone'),
-          subtitle: Text("Great Tablet"),
-          trailing: Icon(Icons.phone_android),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.tablet_android),
+            title: Text('Tablet Phone'),
+            subtitle: Text("Great Tablet"),
+            trailing: Icon(Icons.phone_android),
+          ),
         ),
-        ListTile(
-          leading: Icon(Icons.desktop_mac_outlined),
-          title: Text('Monitor'),
-          subtitle: Text("Beautiful Monitor"),
-          trailing: Icon(Icons.desktop_mac_outlined),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.desktop_mac_outlined),
+            title: Text('Monitor'),
+            subtitle: Text("Beautiful Monitor"),
+            trailing: Icon(Icons.desktop_mac_outlined),
+          ),
         )
       ],
     );
