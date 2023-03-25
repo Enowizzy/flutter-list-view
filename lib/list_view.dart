8a283490dd346d1f780e1a76c6335bd34b3e15e6
Jeedie JideX <enockyona906@gmail.com>
2023-03-25 13:27:06 +0300

    wrap with Padding()

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -11,44 +11,59 @@ class List extends StatelessWidget {
   Widget getListView() {
     var listView = ListView(
       children: const <Widget>[
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.landscape),
-            title: Text('Landscape'),
-            subtitle: Text("Beautiful Landscape"),
-            trailing: Icon(Icons.wb_sunny),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.landscape),
+              title: Text('Landscape'),
+              subtitle: Text("Beautiful Landscape"),
+              trailing: Icon(Icons.wb_sunny),
+            ),
           ),
         ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.laptop_chromebook),
-            title: Text('Laptop'),
-            subtitle: Text("Beautiful Laptop"),
-            trailing: Icon(Icons.laptop),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.laptop_chromebook),
+              title: Text('Laptop'),
+              subtitle: Text("Beautiful Laptop"),
+              trailing: Icon(Icons.laptop),
+            ),
           ),
         ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.phone_android),
-            title: Text('Android Phone'),
-            subtitle: Text("Amazing Phone"),
-            trailing: Icon(Icons.phone_android),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.phone_android),
+              title: Text('Android Phone'),
+              subtitle: Text("Amazing Phone"),
+              trailing: Icon(Icons.phone_android),
+            ),
           ),
         ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.tablet_android),
-            title: Text('Tablet Phone'),
-            subtitle: Text("Great Tablet"),
-            trailing: Icon(Icons.phone_android),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.tablet_android),
+              title: Text('Tablet Phone'),
+              subtitle: Text("Great Tablet"),
+              trailing: Icon(Icons.phone_android),
+            ),
           ),
         ),
-        Card(
-          child: ListTile(
-            leading: Icon(Icons.desktop_mac_outlined),
-            title: Text('Monitor'),
-            subtitle: Text("Beautiful Monitor"),
-            trailing: Icon(Icons.desktop_mac_outlined),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.desktop_mac_outlined),
+              title: Text('Monitor'),
+              subtitle: Text("Beautiful Monitor"),
+              trailing: Icon(Icons.desktop_mac_outlined),
+            ),
           ),
         )
       ],
