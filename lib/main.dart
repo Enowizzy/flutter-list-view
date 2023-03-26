96c08598c72a97e8e62801e4be72f64441962764
Jeedie JideX <enockyona906@gmail.com>
2023-03-26 20:39:11 +0300

    finish add home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,13 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:flutter_list_tile_view/controllers/list_builder.dart';
 import 'package:flutter_list_tile_view/controllers/list_view.dart';
 
 import 'helpers/side_bar.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('List Views'),
-        centerTitle: true,
-        backgroundColor: Colors.indigo,
-         actions: <Widget>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
-      ),
-      drawer: const SideBar(),
-      body: const HomePage(),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        onPressed: () {
-          const ListBuilder();
-        },
-        child: const Icon(Icons.add),
-      ),
-    ),
-  ));
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 }
 
 class HomePage extends StatelessWidget {
@@ -38,6 +17,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const List();
+    // return const List();
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Homepage'),
+            centerTitle: true,
+            backgroundColor: Colors.indigo,
+            actions: <Widget>[
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            ],
+          ),
+          drawer: const SideBar(),
+          body: const List()),
+    );
   }
 }
